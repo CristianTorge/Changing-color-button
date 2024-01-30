5ea0a230fc1359710a8475cf12121148d2d80087
CristianTorge <154727895+CristianTorge@users.noreply.github.com>
2024-01-30 11:53:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 let colors = ['red', 'yellow', 'green'];
   let indexColor = 0;
   const button = document.getElementById('button');
-
   window.onload = function() {
     setInterval(changeColor, 10000);
-    button.addEventListener('click', changeColorOnClick);
+    button.addEventListener('click', changeColor);
   };
-
   function changeColor() {
-    indexColor = (indexColor + 1) % colors.length;
-    button.style.backgroundColor = colors[indexColor];
-  }
-
-  function changeColorOnClick() {
     indexColor = (indexColor + 1) % colors.length;
     button.style.backgroundColor = colors[indexColor];
   }
